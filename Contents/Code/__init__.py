b1d9a2a0d96b9b7d451cf7a7da2cfcbdc9617b8b
Felix Bäcker <fhbspace@gmail.com>
2011-03-23 00:12:40 +0100

    Fix: Film show name added to title as well

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -225,7 +225,7 @@ def formatTitle(series,title,kanal):
 	title=title
 	kanal=kanal
 	var1=False
-	if (kanal == "Aktuell") or (kanal == "Talkshow") or (kanal == "Dokumentation") or (kanal == "Comedy")  or (kanal == "Kinder")  or (kanal == "Krimi")  or (kanal == "Magazin")  or (kanal == "Nachrichten")  or (kanal == "Serie")  or (kanal == "Show")  or (kanal == "Spielfilm")  or (kanal == "Wissen"):	
+	if (kanal == "Aktuell") or (kanal == "Talkshow") or (kanal == "Dokumentation") or (kanal == "Comedy")  or (kanal == "Kinder")  or (kanal == "Krimi")  or (kanal == "Magazin")  or (kanal == "Nachrichten")  or (kanal == "Serie")  or (kanal == "Show")  or (kanal == "Film")  or (kanal == "Wissen"):	
 			m=re.findall('vom [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{2,4}',title)
 			
 			if m != []:
